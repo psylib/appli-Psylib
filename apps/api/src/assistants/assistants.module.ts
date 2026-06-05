46c916d7e99f29c86d9f652a6eaf7cdc632a0655
Ruppel <tony.ruppel7@gmail.com>
2026-06-05 11:45:54 +0200

    feat(assistants): REST controller (invite/list/revoke + accept)

--- apps/api/src/assistants/assistants.module.ts
+++ apps/api/src/assistants/assistants.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AssistantsService } from './assistants.service';
+import { AssistantsController } from './assistants.controller';
 import { AuthModule } from '../auth/auth.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [AuthModule, NotificationsModule],
+  controllers: [AssistantsController],
   providers: [AssistantsService],
   exports: [AssistantsService],
 })
